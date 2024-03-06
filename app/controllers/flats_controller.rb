a4025480a1fa8d7d20c3b38c9ad15437e7f65d48
Florent Vandroy <florentvandroy@gmail.com>
2024-03-06 16:46:19 +0100

    Edit And Update flat controllers/view

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [:show]
+  before_action :set_flat, only: %i[show edit update]
 
   def index
     @flats = Flat.all
@@ -16,6 +16,13 @@ class FlatsController < ApplicationController
   end
 
   def show; end
+
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flats_path
+  end
 
   private
 
